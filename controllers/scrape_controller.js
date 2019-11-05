b54f6288714cda1b83f6c8333b8c9e8a1d74d711
Taco <taco-tues@tuta.io>
2019-11-04 23:39:22 -0700

    format module dependencies

--- controllers/scrape_controller.js
+++ controllers/scrape_controller.js
@@ -1,19 +1,21 @@
-const appRoot  = require('app-root-path');
-const async = require('async');
-const Scrape = require(appRoot + '/models/scrape');
-const pushshift = require(appRoot + '/lib/push-shift');
+const appRoot   =  require('app-root-path');
+const async     =  require('async');
+const pushshift =  require(appRoot + '/lib/push-shift');
+const Scrape    =  require(appRoot + '/models/scrape');
+
 const { body,validationResult } =  require('express-validator');
-const { sanitizeBody } =  require('express-validator');
+const { sanitizeBody }          =  require('express-validator');
 
 
 // Display Scrape create form on GET
 exports.scrape_create_get = function(req, res, next) {
   res.render('scrape_form', 
-    {
+  {
     title: 'Create Scrape'
-    }
+  }
   );
 };
 
+//  Scrape create POST
 exports.scrape_create_post = pushshift.search_by_id_then_get_comments
 
