4f0b9fbe6aa0933cdb21a5b3a0d3779ea63e02a5
Taco <taco-tues@tuta.io>
2019-11-07 15:59:38 -0700

    promise scraper generator works

--- controllers/scrape_controller.js
+++ controllers/scrape_controller.js
@@ -58,39 +58,40 @@ exports.request_url_03 = async function (req, res, next) {
 //#region scrape_create_pushshift_promises_01
 // CURRENT WORK AREA
 
-const search_comments_by_identifier = function (handed_url) {
-  let pushShift_url                =  'https://api.pushshift.io/reddit/';
-  let comment_search_by_identifier =  'comment/search/?link_id=';
-  let search_limit                 =  '&limit=20000';
+// const search_comments_by_identifier = function (handed_url) {
+//   let pushShift_url                =  'https://api.pushshift.io/reddit/';
+//   let comment_search_by_identifier =  'comment/search/?link_id=';
+//   let search_limit                 =  '&limit=20000';
   
-  return pushShift_url + comment_search_by_identifier + handed_url.substring(44, 50) + search_limit;
-};
+//   return pushShift_url + comment_search_by_identifier + handed_url.substring(44, 50) + search_limit;
+// };
 
 // allThere = ((c) => !arr[0].includes(c))();
 
 // incoming req = form fill in req.body.form_response with a value of 
-// 'https://www.reddit.com/r/ethtrader/comments/dsi7h0/a_dexag_story_by_scott_lewis/
+// https://www.reddit.com/r/ethtrader/comments/dsi7h0/a_dexag_story_by_scott_lewis/
 
 
 exports.pushshift_search_by_id_then_get_comments = async function (req, res, next) {
   let errors = validationResult(req);
 
   // let formatted_pushShift_comments = []   // probably not needed
-  
-  let url_container = {
-    request_url: req.body.form_url,
-    formatted_url: pushshift.search_comments_by_identifier(this.request_url)
-      // returns: https://api.pushshift.io/reddit/comment/search/?link_id=7j0ec9&limit=20000
-  }
+
+
+
+  let request_url = req.body.form_response
+  let formatted_url= pushshift.search_comments_by_identifier(request_url)
+    // returns: https://api.pushshift.io/reddit/comment/search/?link_id=7j0ec9&limit=20000
+
 
   let options_01 = {
     method: 'GET',
-    uri: url_container.formatted_url, //api.pushshift.io/reddit/comment/search/?link_id=7j0ec9&limit=20000
+    uri: formatted_url, //api.pushshift.io/reddit/comment/search/?link_id=7j0ec9&limit=20000
     json: true
   }
 
   request(options_01)
-    .then(pushshift.comment_flattener_w_nested_generator())
+    .then(json => pushshift.comment_flattener_w_nested_generator(json))
     .then(json => res.json({ message: json }))
     .catch(err => next(err))
 };
