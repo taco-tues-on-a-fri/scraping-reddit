8b5712c07d7f3e9a25af9435a01afa622559a166
Taco <taco-tues@tuta.io>
2019-11-07 17:09:03 -0700

    final refactor

--- controllers/scrape_controller.js
+++ controllers/scrape_controller.js
@@ -1,14 +1,21 @@
+//|------------------------------------------------------------------------
+//#region | module dependencies
+
 const appRoot   =  require('app-root-path');
 const async     =  require('async');
 const pushshift =  require(appRoot + '/lib/push-shift');
-const request   =  require('request-promise-native');
+const rp        =  require('request-promise-native');
 const Scrape    =  require(appRoot + '/models/scrape');
 
 const { body,validationResult } =  require('express-validator');
 const { sanitizeBody }          =  require('express-validator');
 
-//|--------------------------------------------------------------------------
-//#region Display Scrape create form on GET
+//#endregion
+//|------------------------------------------------------------------------
+
+
+//|------------------------------------------------------------------------
+//#region | Display Scrape create form on GET
 // Works
 
 exports.scrape_create_get = function(req, res, next) {
@@ -20,87 +27,75 @@ exports.scrape_create_get = function(req, res, next) {
 };
 
 //#endregion
-//|--------------------------------------------------------------------------
+//|------------------------------------------------------------------------
 
-//|--------------------------------------------------------------------------
-//#region  Original pushshift build scrape create POST | search_by_id_then_get_comments
+
+
+
+
+//|------------------------------------------------------------------------
+//#region | DISABLED | original pushshift build scrape create POST | search_by_id_then_get_comments
 
 // exports.scrape_create_post = pushshift.search_by_id_then_get_comments
 
 //#endregion
-//|--------------------------------------------------------------------------
+//|------------------------------------------------------------------------
 
 
-//|--------------------------------------------------------------------------
-//#region New Version of Scrape create POST | request_url_03 
-
+//|------------------------------------------------------------------------
+//#region | LIVE | test version of scrape create POST | request_url_03 
 // Works
+
 exports.request_url_03 = async function (req, res, next) {
   let options = {
     method: 'GET',
     uri: req.body.form_response,
-    json: true // Automatically stringifies the body to JSON
+    json: true 
   };
 
-  await request(options)
+  await rp(options)
     .then(json => res.json({ message: json }))
     .catch(err => next(err))
 };
 
 //#endregion
-//|--------------------------------------------------------------------------
+//|------------------------------------------------------------------------
 
 
 
 
 
-//|--------------------------------------------------------------------------
-//#region scrape_create_pushshift_promises_01
+//|------------------------------------------------------------------------
+//#region | LIVE | promise version of scrape create POST | pushshift_search_by_id_then_get_comments
 // CURRENT WORK AREA
-
-// const search_comments_by_identifier = function (handed_url) {
-//   let pushShift_url                =  'https://api.pushshift.io/reddit/';
-//   let comment_search_by_identifier =  'comment/search/?link_id=';
-//   let search_limit                 =  '&limit=20000';
-  
-//   return pushShift_url + comment_search_by_identifier + handed_url.substring(44, 50) + search_limit;
-// };
-
-// allThere = ((c) => !arr[0].includes(c))();
-
-// incoming req = form fill in req.body.form_response with a value of 
-// https://www.reddit.com/r/ethtrader/comments/dsi7h0/a_dexag_story_by_scott_lewis/
-
+/**
+|--------------------------------------------------------------------------
+|  pushshift_search_by_id_then_get_comments
+|--------------------------------------------------------------------------
+| 
+| incoming req = form fill in req.body.form_response with a value url string:
+| https://www.reddit.com/r/ethtrader/comments/dsi7h0/a_dexag_story_by_scott_lewis/
+|
+| formatted_url: https://api.pushshift.io/reddit/comment/search/?link_id=7j0ec9&limit=20000
+}
+*/
 
 exports.pushshift_search_by_id_then_get_comments = async function (req, res, next) {
   let errors = validationResult(req);
 
-  // let formatted_pushShift_comments = []   // probably not needed
-
-
-
   let request_url = req.body.form_response
   let formatted_url= pushshift.search_comments_by_identifier(request_url)
-    // returns: https://api.pushshift.io/reddit/comment/search/?link_id=7j0ec9&limit=20000
 
-
-  let options_01 = {
+  let options = {
     method: 'GET',
-    uri: formatted_url, //api.pushshift.io/reddit/comment/search/?link_id=7j0ec9&limit=20000
+    uri: formatted_url,
     json: true
   }
 
-  request(options_01)
+  rp(options)
     .then(json => pushshift.comment_flattener_w_nested_generator(json))
     .then(json => res.json({ message: json }))
     .catch(err => next(err))
 };
-
-
-  // await request(options)
-  //   .then(json => res.json({ message: json }))
-  //   .catch(err => next(err))
-
-
 //#endregion
-//|--------------------------------------------------------------------------
+//|------------------------------------------------------------------------
