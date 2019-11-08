deb8373436ae9b34f8ffc8e0b014f83db7a52ae8
Taco <taco-tues@tuta.io>
2019-11-07 18:07:36 -0700

    final refactor

--- routes/home.js
+++ routes/home.js
@@ -1,20 +1,31 @@
+//|------------------------------------------------------------------------
+//#region module dependencies
+
 const express =  require('express');
 const router  =  express.Router();
 const appRoot =  require('app-root-path');
-const { body,validationResult } = require('express-validator');
-const { sanitizeBody } = require('express-validator');
+const rp      =  require('request-promise');
+
+const { body,validationResult } =  require('express-validator');
+const { sanitizeBody }          =  require('express-validator');
+
+const home_controller   =  require(appRoot + '/controllers/home_controller');
+const scrape_controller =  require(appRoot + '/controllers/scrape_controller');
+
+//#endregion
+//|------------------------------------------------------------------------
 
 
-
-const home_controller = require(appRoot + '/controllers/home_controller');
-const scrape_controller = require(appRoot + '/controllers/scrape_controller');
-/**
-|--------------------------------------------------------------------------
-| Primary app routes
-|--------------------------------------------------------------------------
-*/
-
-// GET home page 
+// GET | index
+//|------------------------------------------------------------------------
 router.get('/', home_controller.index);
 
+
 module.exports = router;
+
+
+// GET | name
+//|------------------------------------------------------------------------
+
+// POST | name
+//|------------------------------------------------------------------------
