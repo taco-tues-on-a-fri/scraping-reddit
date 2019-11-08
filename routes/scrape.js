4b895be2243c9616b721d2cad1d3d8271ee189c3
Taco <taco-tues@tuta.io>
2019-11-07 17:58:41 -0700

    final refactor

--- routes/scrape.js
+++ routes/scrape.js
@@ -1,22 +1,23 @@
+//|------------------------------------------------------------------------
+//#region module dependencies
+
 const express =  require('express');
 const router  =  express.Router();
 const appRoot =  require('app-root-path');
 
-
 const scrape_controller = require(appRoot + '/controllers/scrape_controller');
 
-// router.get('/', function(req, res, next) {
-//   res.send('Not Implemented');
-// });
+//#endregion
+//|------------------------------------------------------------------------
 
-// GET/POST request for creating a Scrape
-// ! Must come before routes that display Scrape (uses _id).
+
+// GET | scrape_create_get
+//|------------------------------------------------------------------------
 router.get('/scrape/create', scrape_controller.scrape_create_get);
-router.post('/scrape/create', scrape_controller.scrape_create_post);
 
 
-// GET/POST request to delete Scrape
-// router.get('/scrape/:id/delete', scrape_controller.scrape_delete_get);
-// router.post('/scrape/:id/delete', scrape_controller.scrape_delete_post);
+// POST | pushshift_search_by_id_then_get_comments
+//|------------------------------------------------------------------------
+router.post('/scrape/create', scrape_controller.pushshift_search_by_id_then_get_comments);
 
 module.exports = router;
