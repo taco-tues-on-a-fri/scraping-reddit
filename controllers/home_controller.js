d68d361d6f15ee9b11292c9741061faf97b1b491
Taco <taco-tues@tuta.io>
2019-11-08 23:37:35 -0700

    add request_url_fs_save

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,11 +1,11 @@
 //|------------------------------------------------------------------------
 //#region module dependencies
 
-const appRoot      =  require('app-root-path');
-const async        =  require('async')
-const helper       =  require(appRoot + '/lib/helper');
-const rp           =  require('request-promise')
-const Scrape       =  require(appRoot + '/models/scrape');
+const appRoot  =  require('app-root-path');
+const async    =  require('async')
+const helper   =  require(appRoot + '/lib/helper');
+const rp       =  require('request-promise')
+const Scrape   =  require(appRoot + '/models/scrape');
 
 require('express-async-errors');
 //#endregion
@@ -33,6 +33,39 @@ exports.index = function(req, res) {
 };
 //#endregion
 //|------------------------------------------------------------------------
+
+
+
+
+//|------------------------------------------------------------------------
+//#region | TEST |  testing fs helper function | request_url_fs_save
+/**
+|--------------------------------------------------------------------------
+|  request_url_fs_save
+|--------------------------------------------------------------------------
+| 
+|
+*/
+
+exports.request_url_fs_save = async function (req, res, next) {
+  let options = {
+    method: 'GET',
+    uri: 'https://www.reddit.com/r/ethtrader/comments/dtdsmm/ethereum_istanbul_hard_fork_release_date/',
+    json: true
+  };
+
+  let file_name = 'thrilling_woodcock.json' 
+
+
+  await rp(options)
+    .then(json => helper.fs_read_write_02(file_name, json)) 
+    .then(json => res.json({ message: json }))
+    .catch(err => next(err))
+
+};
+//#endregion
+//|------------------------------------------------------------------------
+
 
 
 
