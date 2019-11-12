6f8306c109311e8383d34ed708278348d274a4b4
Taco <taco-tues@tuta.io>
2019-11-12 15:22:19 -0700

    bugs

--- routes/scrape.js
+++ routes/scrape.js
@@ -21,6 +21,15 @@ router.get('/scrape/create', scrape_controller.scrape_create_get);
 router.post('/scrape/create', scrape_controller.regex_pushshift_search_by_id_then_get_comments);
 
 
+// GET | reddit_scrape_create_get
+//|------------------------------------------------------------------------
+router.get('/scrape/reddit_scrape_create', scrape_controller.scrape_create_get);
+
+
+// POST | reddit_scrape_create_POST
+//|------------------------------------------------------------------------
+router.post('/scrape/reddit_scrape_create', scrape_controller.reddit_scrape);
+
 module.exports = router;
 
 
