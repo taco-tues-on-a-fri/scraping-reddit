21d79583b1ab0b9139dcb10ee520fa1f4d8d518e
Taco <taco-tues@tuta.io>
2019-11-15 11:23:06 -0700

    added new scrape form to incorporate

--- routes/scrape.js
+++ routes/scrape.js
@@ -16,36 +16,36 @@ const scrape_controller = require(appRoot + '/controllers/scrape_controller');
 router.get('/scrape/create', scrape_controller.scrape_create_get);
 
 
-// POST | regex_pushshift_search_by_id_then_get_comments //TODO final version - condense this with all other lines with this note.
+// POST | regex_pushshift_search_by_id_then_get_comments //TODO final version - in use - rename to shorter list name
 //|------------------------------------------------------------------------
 router.post('/scrape/create', scrape_controller.regex_pushshift_search_by_id_then_get_comments);
 
 
-// GET | reddit_scrape_create_get //TODO final version - condense this with all other lines with this note.
+// GET | reddit_scrape_create_get //TODO final version - probably delete
 //|------------------------------------------------------------------------
 router.get('/scrape/reddit_scrape_create', scrape_controller.scrape_create_get);
 
 
-// POST | reddit_scrape_create_POST //TODO final version - condense this with all other lines with this note.
+// POST | reddit_scrape_create_POST //TODO final version - In Use - maybe rename to list
 //|------------------------------------------------------------------------
 router.post('/scrape/reddit_scrape_create', scrape_controller.reddit_scrape);
 
-// GET | pushshift_scrape_n_sort_get //TODO final version - condense this with all other lines with this note.
+// GET | pushshift_scrape_n_sort_get //TODO final version - Delete
 //|------------------------------------------------------------------------
 router.get('/scrape/pushshift_scrape_n_sort', scrape_controller.scrape_create_get);
 
 
-// POST | pushshift_scrape_n_sort_POST //TODO final version - condense this with all other lines with this note.
+// POST | pushshift_scrape_n_sort_POST //TODO final version - In use
 //|------------------------------------------------------------------------
 router.post('/scrape/pushshift_scrape_n_sort', scrape_controller.pushshift_scrape_n_sort_post);
 module.exports = router;
 
-// GET | reddit_scrape_n_sort_get //TODO final version - condense this with all other lines with this note.
+// GET | reddit_scrape_n_sort_get //TODO final version - Delete
 //|------------------------------------------------------------------------
 router.get('/scrape/reddit_scrape_n_sort', scrape_controller.scrape_create_get);
 
 
-// POST | reddit_scrape_n_sort_POST  //TODO final version - condense this with all other lines with this note.
+// POST | reddit_scrape_n_sort_POST  //TODO final version - In use
 //|------------------------------------------------------------------------
 router.post('/scrape/reddit_scrape_n_sort', scrape_controller.reddit_scrape_n_sort_post);
 module.exports = router;
